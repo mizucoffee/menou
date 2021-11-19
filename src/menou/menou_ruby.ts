3945575d6167356bfe16a916596ff74420312d95
Ritsuki Goto <develop@mizucoffee.net>
2021-11-20 03:10:05 +0900

    [fix] spawn

--- src/menou/menou_ruby.ts
+++ src/menou/menou_ruby.ts
@@ -1,12 +1,25 @@
 import { Menou } from "./menou";
 import { spawn } from "../tools";
+import { spawnSync, SpawnOptionsWithoutStdio } from "child_process";
 
 export class MenouRuby extends Menou {
+  opts: SpawnOptionsWithoutStdio = {};
+
+  constructor() {
+    super();
+    this.opts = {
+      cwd: this.repoDir,
+      env: {
+        ...process.env,
+        DATABASE_URL: `${process.env.DATABASE_URL?.replace(/menou-next/g, `menou-${this.id}`)}`
+      }
+    };
+  }
+
   async migrate() {
-    const DATABASE_URL = process.env.DATABASE_URL?.replace(/menou-next/g, `menou-${this.id}`)
-    const opt = { cwd: this.repoDir, env: {DATABASE_URL} };
-    await spawn('bundle', [], opt, console.log, console.error)
-    await spawn('bundle', ['exec', 'rake', 'db:migrate'], opt, console.log, console.error)
+    await spawn('bundle', [], this.opts, console.log, console.error)
+    await spawn('bundle', ['exec', 'rake', 'db:create'], this.opts, console.log, console.error)
+    await spawn('bundle', ['exec', 'rake', 'db:migrate'], this.opts, console.log, console.error)
   }
   check_schema() {
 
--- src/tools.ts
+++ src/tools.ts
@@ -9,15 +9,19 @@ export function checkProperty(target: { [key: string]: string; }, properties: st
   })
 }
 
-export function spawn (cmd: string, args: string[], options: child_process.SpawnOptionsWithoutStdio, stdout?: (data: string) => void, stderr?: (data: string) => void) {
+export function spawn (cmd: string, args: string[], options: child_process.SpawnOptionsWithoutStdio, stdout?: (data: string) => void, stderr?: (data: string) => void):Promise<string> {
   return new Promise((res, rej)=>{
     const process = child_process.spawn(cmd, args, options);
     process.stdout.setEncoding('utf-8');
     process.stderr.setEncoding('utf-8');
-    if(stdout) process.stdout.on('data', stdout);
+    let result = "";
+    process.stdout.on('data', (data) => {
+      result += data;
+      if(stdout) stdout(data)
+    });
     if(stderr) process.stderr.on('data', stderr);
     process.on('exit', code => {
-      if (code == 0) res(null)
+      if (code == 0) res(result)
       else rej(code)
     });
   })
